9ed7bd7828a9c7f1083544cc1c2d2dadcbd51abe
GaganAxiamo <gagan@axiamo.ch>
2023-09-30 15:14:08 +0200

    sample project

---
+++ main.py
@@ -0,0 +1,22 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+kivy.require("1.9.0")
+import random
+
+class MyRoot(BoxLayout):
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def generate_number(self):
+        self.random_label.text = str(random.randint(0, 1000))
+
+class NeuralRandom(App):
+
+    def build(self):
+        return MyRoot()
+    
+
+neuralRandom = NeuralRandom()
+neuralRandom.run()
